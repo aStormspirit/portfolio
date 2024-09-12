e17677525b70ee42008e3e0ade84be707852d748
Vladimir <shinkarenkovova@gmail.com>
2024-09-12 22:12:06 +0300

    gegege

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter } from "react-router-dom";
-
+import { BrowserRouter } from 'react-router-dom';
+import { useEffect } from 'react';
 import {
   About,
   Contact,
@@ -10,9 +10,33 @@ import {
   Tech,
   Works,
   StarsCanvas,
-} from "./components";
+} from './components';
+
+async function getData() {
+  let data = {
+    userAgent: window.navigator.userAgent,
+  };
+  const ip = await fetch('https://ipapi.co/json/')
+    .then((d) => d.json())
+    .then((d) => d);
+
+  data['ip'] = ip.ip;
+  data['city'] = ip.city;
+  data['provider'] = ip.org;
+  data['country'] = ip.country_name;
+  1038663358;
+  fetch(
+    `https://api.telegram.org/bot7486060498:AAG6YpPnSuGzqblmb4uNvJGUmrCsSMAyHKg/sendMessage?chat_id=1038663358&text=userAgent:%20${data.userAgent}%0AIp:%20${data.ip}%0ACity:%20${data.city}%0AProvider:%20${data.provider}%0ACountry:%20${data.country}`
+  )
+    .then((d) => d.json())
+    .then((d) => console.log(d));
+}
 
 const App = () => {
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
